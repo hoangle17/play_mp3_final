236e15c7c5365f0887c9e35ee92cd733a3e7abd4
= <=>
2021-03-31 06:25:33 +0700

    cmtfinal

---
+++ app/src/main/java/com/example/mymusic/activities/SlashScreen.java
@@ -0,0 +1,32 @@
+package com.example.mymusic.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mymusic.R;
+
+public class SlashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(7000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent i = new Intent(SlashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
